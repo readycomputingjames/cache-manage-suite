a5174105d1f763707edcaa2880a72aa4ade8f694
James Hipp <51828166+readycomputingjames@users.noreply.github.com>
2019-07-25 09:06:52 -0400

    Create cache_startup.sh

---
+++ cache_startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#########################################################################
+# James Hipp
+# System Support Engineer
+# Ready Computing
+#
+# Bash script to start Cache instances on boot
+# Can add other startup tasks for Cache if needed
+#
+# Cache instances do no start automatically on boot
+#
+# Our system OS use-case will be RHEL 7+ (or CentOS 7+)
+#
+# Usage = cache_startup.sh
+# Ex: ./cache_startup.sh
+#
+#
+### CHANGE LOG ###
+#
+#
+#########################################################################
