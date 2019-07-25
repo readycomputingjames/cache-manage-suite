97bb9bc9412963c528c71ca2d648ac6e524bc1f2
James Hipp <51828166+readycomputingjames@users.noreply.github.com>
2019-07-25 09:29:18 -0400

    Update cache_startup.sh

--- cache_startup.sh
+++ cache_startup.sh
@@ -7,6 +7,8 @@
 # Bash script to start Cache instances on boot
 # Can add other startup tasks for Cache if needed
 #
+# Since this is a startup script, we will assume Cache is installed
+#
 # Cache instances do no start automatically on boot
 #
 # Our system OS use-case will be RHEL 7+ (or CentOS 7+)
@@ -19,3 +21,45 @@
 #
 #
 #########################################################################
+
+start_instances()
+{
+
+   # Load Instances into an Array, in case we have Multiple
+   instances=()
+   while IFS= read -r line; do
+      instances+=( "$line" )
+   done < <( sudo ccontrol list |grep Configuration |awk '{ print $2 }' |tr -d "'" )
+
+   for i in ${instances[@]};
+   do
+      sudo ccontrol start $i > /dev/null 2>&1
+   done
+
+}
+
+is_up()
+{
+
+   # Load Instances into an Array, in case we have Multiple
+   instances=()
+   while IFS= read -r line; do
+      instances+=( "$line" )
+   done < <( sudo ccontrol list |grep Configuration |awk '{ print $2 }' |tr -d "'" )
+
+   for i in ${instances[@]};
+   do
+      
+   done
+
+}
+
+main()
+{
+
+
+
+}
+
+
+
