0864a895c7a3b1e2c9ef56c4f30c5bc7d9171431
James Hipp <51828166+readycomputingjames@users.noreply.github.com>
2019-08-05 12:14:03 -0400

    Update cache_manage.sh

--- cache_manage.sh
+++ cache_manage.sh
@@ -27,7 +27,7 @@
 # 20190729 = Added Auth-Enabled Flag
 # 20190730 = Changed 'sudo ccontrol...' to '/usr/bin/ccontrol...'
 # 20190730 = Changed 'csession...' to '/usr/bin/csession...'
-# 20190805 = Added Show-Namespaces  Flag
+# 20190805 = Added List-Namespaces  Flag
 # 20190805 = Added Show-App-Errors Flag
 #
 #########################################################################
